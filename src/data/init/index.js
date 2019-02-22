06d5127c5b7f64cd981e51089d263a9f5043e34e
yangxi <yangxi@babytree-inc.com>
2019-02-22 10:39:55 +0800

    feat: 添加adk模版

--- src/data/init/index.js
+++ src/data/init/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     githubTemplates: {
+        'mona-template-sdk': 'monajs/mona-template-sdk',
         'mona-template-react': 'monajs/mona-template-react',
         'react-h5-template': 'func-star/react-h5-template',
         'vue-h5-template': 'func-star/vue-h5-template'
